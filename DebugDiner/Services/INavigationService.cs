4cb7308253e0f0c0ff13be40c4d5e8bc0f2b95aa
Gravia Daemon <graviaRotterdam@gmail.com>
2026-04-06 11:29:26 +0200

    Adding in navigation

---
+++ DebugDiner/Services/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿using Terminal.Gui;
+
+namespace DebugDiner.Services;
+
+public interface INavigationService
+{
+    public void NavigateTo<TView>() where TView : View;
+    public void SetContentArea(View contentArea);
+}
---
+++ DebugDiner/Services/NavigationService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Terminal.Gui;
+
+namespace DebugDiner.Services;
+
+public class NavigationService(IServiceProvider services) : INavigationService
+{
+    private View? _contentArea;
+    public void SetContentArea(View contentArea) => _contentArea = contentArea;
+
+    public void NavigateTo<TView>()
+        where TView : View
+    {
+        if (_contentArea is null)
+        {
+            return;
+        }
+
+        foreach (var sub in _contentArea.Subviews.ToList())
+        {
+            _contentArea.Remove(sub);
+        }
+
+        var view = services.GetRequiredService<TView>();
+        view.X = 0;
+        view.Y = 0;
+        view.Width = Dim.Fill();
+        view.Height = Dim.Fill();
+
+        _contentArea.Add(view);
+        _contentArea.SetNeedsDraw();
+    }
+}
