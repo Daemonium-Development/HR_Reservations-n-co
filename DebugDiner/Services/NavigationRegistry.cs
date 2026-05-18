588b2f9424c671b4ea5caf509e4575b6774adf05
SoufianManai <s.manai@like-it.nl>
2026-05-18 11:11:35 +0200

    Code consistency

--- DebugDiner/Services/NavigationRegistry.cs
+++ DebugDiner/Services/NavigationRegistry.cs
@@ -4,8 +4,7 @@ namespace DebugDiner.Services;
 
 public static class NavigationRegistry
 {
-    private static Action<INavigationService> Logout =>
-        n => { AppState.CurrentUser = null; n.ClearHistory(); n.NavigateTo<WelcomeView>(); };
+    private static Action<INavigationService> Logout => n => { AppState.CurrentUser = null; n.ClearHistory(); n.NavigateTo<WelcomeView>(); };
 
     private static readonly Dictionary<Type, Func<INavigationService, IEnumerable<NavigationItem>>> _registry = new()
     {
@@ -30,56 +29,58 @@ public static class NavigationRegistry
         {
             var items = new List<NavigationItem>
             {
-                new("Home",                 n => n.NavigateTo<HomeView>()),
-                new("Make a Reservation",   n => n.NavigateTo<CreateReservationsView>()),
-                new("View my Reservations", n => n.NavigateTo<ReservationsView>()),
-                new("User Information",     n => n.NavigateTo<InformationView>()),
-                new("Menu (Dishes)",        n => n.NavigateTo<DishView>()),
+                new NavigationItem("Home",                 n => n.NavigateTo<HomeView>()),
+                new NavigationItem("Make a Reservation",   n => n.NavigateTo<CreateReservationsView>()),
+                new NavigationItem("View my Reservations", n => n.NavigateTo<ReservationsView>()),
+                new NavigationItem("User Information",     n => n.NavigateTo<InformationView>()),
+                new NavigationItem("Menu (Dishes)",        n => n.NavigateTo<DishView>()),
             };
 
             if (AppState.CurrentUser?.Role == Role.Admin)
             {
                 items.AddRange([
-                    new("Create Dish",        n => n.NavigateTo<CreateDishView>()),
-                    new("Users",              n => n.NavigateTo<AdminUsersView>()),
-                    new("Add User",           n => n.NavigateTo<CreateUserView>()),
-                    new("Admin Reservations", n => n.NavigateTo<ReservationsView>()),
+                    new NavigationItem("Create Dish",        n => n.NavigateTo<CreateDishView>()),
+                    new NavigationItem("Users",              n => n.NavigateTo<AdminUsersView>()),
+                    new NavigationItem("Add User",           n => n.NavigateTo<CreateUserView>()),
+                    new NavigationItem("Admin Reservations", n => n.NavigateTo<ReservationsView>()),
                 ]);
             }
 
-            items.Add(new("Logout", Logout));
+            items.Add(new NavigationItem("Logout", Logout));
             return items;
         },
 
         [typeof(ReservationsView)] = _ =>
         [
-            new("Home",               n => n.NavigateTo<HomeView>()),
-            new("Make a Reservation", n => n.NavigateTo<CreateReservationsView>()),
-            new("Logout",             Logout),
+            new NavigationItem("Home",               n => n.NavigateTo<HomeView>()),
+            new NavigationItem("Make a Reservation", n => n.NavigateTo<CreateReservationsView>()),
+            new NavigationItem("Logout",             Logout),
         ],
 
         [typeof(InformationView)] = _ =>
         [
-            new("Home",                 n => n.NavigateTo<HomeView>()),
-            new("Make a Reservation",   n => n.NavigateTo<CreateReservationsView>()),
-            new("View my Reservations", n => n.NavigateTo<ReservationsView>()),
-            new("Logout",               Logout),
+            new NavigationItem("Home",                 n => n.NavigateTo<HomeView>()),
+            new NavigationItem("Make a Reservation",   n => n.NavigateTo<CreateReservationsView>()),
+            new NavigationItem("View my Reservations", n => n.NavigateTo<ReservationsView>()),
+            new NavigationItem("Logout",               Logout),
         ],
 
         [typeof(DishView)] = _ =>
         [
-            new("Home",   n => n.NavigateTo<HomeView>()),
-            new("Logout", Logout),
+            new NavigationItem("Home",   n => n.NavigateTo<HomeView>()),
+            new NavigationItem("Logout", Logout),
         ],
 
         [typeof(AdminUsersView)] = _ =>
         [
-            new("Home",     n => n.NavigateTo<HomeView>()),
-            new("Add User", n => n.NavigateTo<CreateUserView>()),
-            new("Logout",   Logout),
+            new NavigationItem("Home",     n => n.NavigateTo<HomeView>()),
+            new NavigationItem("Add User", n => n.NavigateTo<CreateUserView>()),
+            new NavigationItem("Logout",   Logout),
         ],
     };
 
-    public static IEnumerable<NavigationItem> GetItemsFor(Type viewType, INavigationService nav) =>
-        _registry.TryGetValue(viewType, out var factory) ? factory(nav) : [];
+    public static IEnumerable<NavigationItem> GetItemsFor(Type viewType, INavigationService nav)
+    {
+        return _registry.TryGetValue(viewType, out var factory) ? factory(nav) : [];
+    }
 }
